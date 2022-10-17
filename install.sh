d7589594a074ba7eddb2e89f1e3e439fd9635da5
olezha baranov <95653526+krovostok@users.noreply.github.com>
2022-10-17 20:36:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ echo "installing dscharrer's steamwm...
 "
 killall -q -9 steam
 sleep 2
-#mv ~/steamwm-auto ~/.steamwm-auto
+mv ~/steamwm-auto ~/.steamwm-auto
+mkdir ~/.steam/steam/skins
+cp -r ~/.steamwm-auto/"< no buttons >" ~/.steam/steam/skins
 chmod +x steamwm.cpp
 ./steamwm.cpp
 
@@ -15,4 +17,3 @@ echo "installation complete!
 
 enjoy! -olezha
 "
-cd ~
