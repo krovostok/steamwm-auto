7732e34185de2489ccd1cf5d45b6fa1ced273d45
olezha baranov <95653526+krovostok@users.noreply.github.com>
2022-10-17 18:52:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+clear
 echo "installing dscharrer's steamwm...
 "
 killall -q -9 steam
-sleep 1
+sleep 2
 #mv ~/steamwm-auto ~/.steamwm-auto
 chmod +x steamwm.cpp
 ./steamwm.cpp
 
 clear
 echo "installation complete!
-
 'steam (runtime) with borders' menu entry was created.
 
-enjoy! 
-
-- krovostok"
+enjoy! -olezha"
